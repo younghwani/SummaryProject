3eb53c9f7829168a215f8b3d5fc9d88c0daa7159
younghwani <younghwankim624@gmail.com>
2022-01-25 23:30:58 +0900

    hide About page

--- React/summarize-react/src/App.js
+++ React/summarize-react/src/App.js
@@ -4,16 +4,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Main from './container/Main';
-import About from './container/About';
+// import About from './container/About';
 import Result from './container/Result';
 
 export default function App() {
 	return (
 		<Router>
 			<div>
-				<Route exact path="/" component={Main} />
+				<Route path="/" component={Main} />
 				<Route exact path="/result/:result" component={Result} />
-				<Route exact path="/about" component={About} />
+				{/* <Route exact path="/about" component={About} /> */}
 			</div>
 		</Router>
 	);
--- React/summarize-react/src/container/About.js
+++ React/summarize-react/src/container/About.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './About.css';
+
+// function githubClick() {
+// 	window.location.href = `https://github.com/younghwani`;
+// }
 
 export default function About() {
-	return <div>About</div>;
+	return (
+		<div className="aboutContainer">
+			<h1>About</h1>
+			<h2>
+				Copyright by{' '}
+				<Link
+					to={{ pathname: 'https://github.com/younghwani' }}
+					target="_blank"
+				>
+					Younghwani
+				</Link>
+			</h2>
+		</div>
+	);
 }
