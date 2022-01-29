8a797ac08920f4312b8ee274af23ebf63ecaf351
younghwani <younghwankim624@gmail.com>
2022-01-29 13:45:14 +0900

    Footer link 추가

--- React/summarize-react/src/container/Footer.js
+++ React/summarize-react/src/container/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => (
 	<div className="footer">
-		<div className="copyright">© Copyright by Younghwani</div>
+		<div className="copyright">
+			© Copyright by{' '}
+			<Link
+				to={{ pathname: 'https://github.com/younghwani' }}
+				target="_blank"
+				style={{ color: 'black' }}
+			>
+				Younghwani
+			</Link>
+		</div>
 	</div>
 );
 
